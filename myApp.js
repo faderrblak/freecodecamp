ee0d224608473f6dfbbb69eb0a650c082ccf8223
Glitch (tremendous-materialistic-mountain) <none>
2020-06-05 17:03:14 +0000

    💽🍍 Updated with Glitch

--- myApp.js
+++ myApp.js
@@ -1,6 +1,12 @@
-
+console.log("Hello World!")
 var express = require('express');
 var app = express();
+
+app.get('/', function(req, res) {
+  res.sendFile(__dirname + '/views/index.html');
+})
+
+app.use(express.static(__dirname + '/public'))
 
 // --> 7)  Mount the Logger middleware here
 
